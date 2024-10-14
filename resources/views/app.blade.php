a6e60c458431693fbe546b01404aca0ce80cdcd2
caclm10 <lewinxander@gmail.com>
2024-10-14 18:05:26 +0700

    feat: add page meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
+    <title inertia>{{ config('app.name', 'Mini ToDoList') }}</title>
+    <meta name="description" content="Just a simple and minimalistic To-Do List App.">
+
     @viteReactRefresh
     @vite('resources/js/app.jsx')
 
